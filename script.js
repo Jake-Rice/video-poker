b5a36b6d956f6b0fa407db32240263a24300e076
Jake-Rice <jrice189@gmail.com>
2021-10-26 16:28:12 -0700

    Improved button design and added betting logic

---
+++ script.js
@@ -0,0 +1,67 @@
+import Deck, { Card } from './deck.js'
+
+const MAX_BET = 5
+
+let deck, hand, holds
+let holdsActive = false
+let credits = 2000
+let bet = 0
+
+document.getElementById('btn-bet').addEventListener('click', () => {
+    increaseBet()
+})
+
+document.getElementById('btn-max').addEventListener('click', () => {
+    betMax()
+})
+
+document.getElementById('btn-dd').addEventListener('click', () => {
+    if (bet===0) {
+        betMax()
+    }    
+    else startGame()
+})
+
+for (let i=0; i<5; i++) {
+    document.getElementById('btn-hold'+i).addEventListener('click', () => {
+        if (holdsActive) {
+            holds[i] = !holds[i];
+            document.getElementById('hold'+i).style = (holds[i]) ? '' : 'display: none'
+        }
+    })
+}
+
+function increaseBet() {
+    if (bet < MAX_BET && credits > bet) bet++
+    document.getElementById('bet-label').innerText = "BET " + bet
+    highlightPaytable()
+}
+
+function betMax() {
+    if (credits >= MAX_BET) {
+        bet = MAX_BET
+    }
+    else bet = credits
+
+    document.getElementById('bet-label').innerText = "BET " + bet
+    highlightPaytable()
+
+    if (bet!==0) startGame()
+}
+
+function highlightPaytable() {
+    document.getElementById('payout'+bet).style = "background-color: red;"
+    if (bet>1) document.getElementById('payout'+(bet-1)).style = ""
+}
+
+function startGame() {
+    credits -= bet;
+    document.getElementById('credit-label').innerText = "CREDITS " + credits
+    holds = [false, false, false, false, false]
+    deck = new Deck()
+    deck.shuffle()
+    hand = [deck.draw(), deck.draw(), deck.draw(), deck.draw(), deck.draw()]
+    holdsActive = true;
+    
+    console.log(hand)
+}
